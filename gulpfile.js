13d3ecf13764b111207e418134efdefbfdfad670
ZachDenholm <Zach@zacharys-MacBook-Pro.local>
2016-11-03 12:27:51 +0000

    fixes and updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-﻿var gulp = require('gulp');
+﻿'use strict';
+
+var gulp = require('gulp');
 var sass = require('gulp-sass');
-var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 
-gulp.task('styles', function() {
-    gulp.src('scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(minify())
-        .pipe(gulp.dest('./css/'));
+var scssFiles = 'scss/**/*.scss';
 
+var cssFiles = 'css/**/*.css';
+var cssPath = 'css';
+
+gulp.task('scss', function() {
+    return gulp.src(scssFiles)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./' + cssPath));
+});
+
+gulp.task('css', function() {
+    gulp.src(cssFiles)
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(rename('GovJE.min.css'))
+        .pipe(gulp.dest(cssPath));
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function() {
+    gulp.watch(scssFiles, ['scss']); 
+    gulp.watch(cssFiles, ['css']);
 });
