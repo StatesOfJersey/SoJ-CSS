04d88d1e450a2ff59602ed5a9b176b420d864589
ZachDenholm <zach@fusion.je>
2018-10-24 15:54:13 +0100

    fixing issue with gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,31 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
-var watch = require('gulp-watch');
+var cleanCSS = require('gulp-clean-css');
 
 var scssFiles = 'scss/**/*.scss';
 var cssFiles = 'css/**/*.css';
 
-var cssPath = 'css';
-var destPath = 'dist';
+var cssPath = './css';
+var destPath = './dist';
 
 gulp.task('build', function() {
-    gulp.src(scssFiles)
+   return gulp.src(scssFiles)
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest(cssPath));
 });
 
 gulp.task('minify', function() {
-    gulp.src(cssFiles) 
+    return gulp.src(cssFiles) 
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(minifyCSS({ keepSpecialComments: 0 }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename('GovJE.min.css'))
         .pipe(gulp.dest(destPath));
 });
 
 gulp.task('default', function() {
     gulp.watch(scssFiles,gulp.series('build'));
-    gulp.watch(scssFiles,gulp.series('minify'));
+    gulp.watch(cssFiles,gulp.series('minify'));
 });
